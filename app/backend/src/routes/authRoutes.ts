25b19077f82ccf3e8cdb32b46f4b3c0c00fe1cae
SPEED OF LIGHT <mac@MacBookPro.lan>
2026-05-12 10:31:52 +0100

    add auth routes

--- app/backend/src/routes/authRoutes.ts
+++ app/backend/src/routes/authRoutes.ts
@@ -6,4 +6,16 @@ const authRouter = Router();
 // POST /api/v1/auth/credential-signup
 authRouter.post('/credential-signup', AuthController.credentialSignup);
 
+// POST /api/v1/auth/verify-account
+authRouter.post('/verify-account', AuthController.verifyAccount);
+
+// POST /api/v1/auth/login
+authRouter.post('/login', AuthController.loginWithMagicLink);
+
+// POST /api/v1/auth/verify-login-link
+authRouter.post('/verify-login-link', AuthController.verifyLoginlink);
+
+// POST /api/v1/auth/resend-otp
+authRouter.post('/resend-otp', AuthController.resendOTP);
+
 export default authRouter;
